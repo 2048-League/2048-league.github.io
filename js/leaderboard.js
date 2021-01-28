bac55f6f1bf20f77d8f52289173600f3bb2845e9
Bob Bob <turtles142857@gmail.com>
2021-01-28 16:40:25 +0000

    stuff

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,11 +1,11 @@
  fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/leaderboard', {
     method: 'GET'
  })
-  .then(res => res.json)
+  .then(res => res.json())
   .then(body => {
-    document.getElementById('leaderboardText').innerHTML = `1. ${body[0]}
-    2. ${body[1]}
-    3. ${body[2]}
-    4. ${body[3]}
+    document.getElementById('leaderboardText').innerHTML = `1. ${body[0]}<br>
+    2. ${body[1]}<br>
+    3. ${body[2]}<br>
+    4. ${body[3]}<br>
     5. ${body[4]}`
   });
