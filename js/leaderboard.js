51283e37e52718069d1a3654138875fa23f18ca3
CubeyTheCube <72284516+CubeyTheCube@users.noreply.github.com>
2021-01-28 11:31:47 -0500

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,7 +1,7 @@
  fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/leaderboard', {
     method: 'GET'
  })
-  .then(res => res.json)
+  .then(res => res.json())
   .then(body => {
     document.getElementById('leaderboardText').innerHTML = `1. ${body[0]}
     2. ${body[1]}
