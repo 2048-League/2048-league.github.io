3c9f40dfb001159169b561251989acf1a3f55ea5
CubeyTheCube <72284516+CubeyTheCube@users.noreply.github.com>
2021-01-28 13:18:34 -0500

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -3,9 +3,9 @@
  })
   .then(res => res.json())
   .then(body => {
-    document.getElementById('leaderboardText').innerHTML = `1. ${body[0]}<br>
-    2. ${body[1]}<br>
-    3. ${body[2]}<br>
-    4. ${body[3]}<br>
-    5. ${body[4]}`
+    document.getElementById('leaderboardText').innerHTML = `1. ${body[0].score} at ${body[0].time}<br>
+    2. ${body[1].score} at ${body[1].time}<br>
+    3. ${body[2].score} at ${body[2].time}<br>
+    4. ${body[3].score} at ${body[3].time}<br>
+    5. ${body[4].score} at ${body[4].time}`
   });
