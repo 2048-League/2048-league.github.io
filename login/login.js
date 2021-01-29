b48f7d80b42ac87fcf9f58c8c47df68f71f80fec
Zixuan Wang <wzixuanCC@gmail.com>
2021-01-29 23:32:38 +0000

    P.U.S.H.

---
+++ login/login.js
@@ -0,0 +1,9 @@
+function login() {
+  fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/signup', {
+  method: "POST",
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ "username": document., "password": "kmaoaa", "password2": "kmaoaa", "email": "a@b" })
+  });
+}
--- signup/signup.js
+++ signup/signup.js
@@ -4,7 +4,18 @@ function signup() {
   headers: {
   'Content-Type': 'application/json'
   },
-  body: JSON.stringify({ "username": document.getElementById('username').value, "password": document.getElementById('password').value, "password2": document.getElementById('confirm-password').value, "email": document.getElementById('email').value })
+  body: JSON.stringify({ 
+    "username": document.getElementById('username').value, 
+    "password": document.getElementById('password').value, 
+    "password2": document.getElementById('confirm-password').value, 
+    "email": document.getElementById('email').value })
+  })
+  .then(res => res.text())
+  .then(res => {
+  if(res == "Success!") {
+    window.location = "../";
+  } else {
+    document.getElementById('error').innerHTML = res;
+  }
   });
-  window.location = "../";
 }
