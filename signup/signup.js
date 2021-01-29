ce5d0d219325beca2ac0a57bbc94bf36a2701136
Zixuan Wang <wzixuanCC@gmail.com>
2021-01-29 23:17:16 +0000

    login and signup

--- signup/signup.js
+++ signup/signup.js
@@ -1,3 +1,10 @@
 function signup() {
+  fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/signup', {
+  method: "POST",
+  headers: {
+  'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ "username": document.getElementById('username').value, "password": document.getElementById('password').value, "password2": document.getElementById('confirm-password').value, "email": document.getElementById('email').value })
+  });
   window.location = "../";
 }
