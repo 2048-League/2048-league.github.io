5a7d0084ad9173fc1eb29b0686e29599ca810bbd
Bob Bob <turtles142857@gmail.com>
2021-02-15 21:02:31 +0000

    a

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,4 +1,3 @@
-var rank;
 var leaderboardType = localStorage.getItem("leaderboardType")
 const el = document.getElementById('leaderboard');
 
@@ -24,7 +23,7 @@ const measureText = text => {
   return ctx.measureText(text).width / 5.5;
 }
 
-function leaderboard() {
+function leaderboard(ltype=leaderboardType) {
 fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/leaderboard', {
     method: 'GET'
  })
@@ -33,9 +32,9 @@ fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/leaderboard', {
     Array.from(document.getElementById('leaderboardbox').children).forEach((element, index) => {
       element.style.backgroundColor = colors[index];
     });
-    if(leaderboardType == 'day') {
+    if(ltype == 'day') {
       body = body.filter((item) => (Date.now() - Date.parse(item.time)) < 86400000)
-    } else if(leaderboardType == 'week') {
+    } else if(ltype == 'week') {
       body = body.filter((item) => (Date.now() - Date.parse(item.time)) < 86400000 * 7)
     }
     body = body.filter((item, index) => {
@@ -45,14 +44,24 @@ fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/leaderboard', {
     });
     for(var i = 0; i < 10; i++) {
       let text = body[i] ? body[i].score + ' - ' + body[i].username : '';
+      if(ltype == leaderboardType) {
       document.getElementById('leaderboard' + (i + 1)).style.fontSize = (measureText(text) < 18 ? 18 : 18 - (measureText(text) - 18)) + 'px'; 
       document.getElementById('leaderboard' + (i + 1)).innerHTML = text;
+      }
+    }
+    if(ltype == leaderboardType) {
+    document.getElementById('ranku').style.backgroundColor = '#000';
+    document.getElementById('ranku').innerHTML = '11';
+    document.getElementById('leaderboardu').innerHTML = '';
     }
 
     body.some((item, index) => {
       if(item.username == localStorage.username) {
-        rank = index + 1;
+        if(ltype == 'all') {
+        document.getElementsByClassName('leaderboardtile')[24].innerHTML = index + 1;
         localStorage.setItem('bestScore', item.score);
+        }
+      if(ltype == leaderboardType) {
         if(index < 10) {
           let text = body[10] ? body[10].score + ' - ' + body[10].username : '';
           document.getElementById('leaderboardu').style.fontSize = (measureText(text) < 18 ? 18 : 18 - (measureText(text) - 18)) + 'px'; 
@@ -65,13 +74,14 @@ fetch('https://2048GrandMastersBackend.cubeythecube.repl.co/leaderboard', {
           document.getElementById('ranku').style.backgroundColor = '#F00';
           document.getElementById('leaderboardu').innerHTML = text;          
         }
+      }
         return true;
       }
       return false;
     });
 });
+return document.getElementsByClassName('leaderboardtile')[24].innerHTML;
 }
-leaderboard();
 
 function change() {
   const el = document.getElementById('leaderboard');
